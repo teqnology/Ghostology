4d035eb5d68955473547cb5ab03f1ec5be136525
Teqnology <as.armada@gmail.com>
2014-11-10 16:20:55 +1100

    updated to use foundation and styling based on lifeaweso.me

---
+++ assets/js/main.js
@@ -0,0 +1,73 @@
+
+$(document).ready(function(){
+	
+	
+	/*/ --- CONTENT --- /*/
+	function centerContent(section){
+		var $section = section;
+		var $content = $section.find('.wrapper');
+		
+		var sectionH = 	$section.height();
+		var contentH =	$content.height();
+		var padding =	(sectionH - contentH) / 2 - 10;
+		
+		$content.css({
+			'padding-top' : padding
+		});
+		
+	}
+	
+	$('section').each(function(){
+		centerContent($(this));
+	});
+	
+	
+	
+	/*/ --- SCROLL TO --- /*/
+	$('.down').click(function(){
+		var destination = $(this).attr('data-destination');
+		$.scrollTo(destination, 800, {easing: 'easeInOutExpo'});
+	});
+	$('.logo').click(function(){
+		$.scrollTo(0, 800, {easing: 'easeInOutExpo'});
+	});
+	$('#header nav ul li').click(function(){
+		var destination = $(this).attr('data-destination');
+		$.scrollTo(destination, 800, {easing: 'easeInOutExpo'});
+	});
+	
+	
+	/*/ --- LIFEAWESO.ME --- /*/
+	$('.lifeawesome').each(function(){
+		$(this).text('lifeaweso.me');
+	});
+	
+	
+	/*/ --- LINK FOR TWITTER --- /*/
+	$.fn.tweetify = function() {
+		this.each(function() {
+			$(this).html(
+				$(this).html()
+					.replace(/((ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?)/gi,'<a href="$1">$1</a>')
+					.replace(/(^|\s)#(\w+)/g,'$1<a href="http://search.twitter.com/search?q=%23$2">#$2</a>')
+					.replace(/(^|\s)@(\w+)/g,'$1<a href="http://twitter.com/$2">@$2</a>')
+			);
+		});
+		return $(this);
+	};
+	$("#twitter-feed").tweetify();
+	
+	
+	
+	
+	
+	
+	$(window).resize(function(){
+		$('section').each(function(){
+			centerContent($(this));
+		});
+	});
+	
+	
+	
+});
